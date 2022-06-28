4f69186c7812605b7ffd3ede77cd7f7797d75598
Andrea <90292543+amgallo1213@users.noreply.github.com>
2022-06-27 21:18:19 -0400

    Added a new answer

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var answers = ["It is certain", "It is decidedly so", "Without a doubt",
                 "Yes - definitely", "You may rely on it", "As I see it, yes", "Most likely",
                 "Outlook good", "Yes", "Signs point to yes", "Don't count on it", "My reply is no",
                 "My sources say no", "Outlook not so good", "Very doubtful", "Reply hazy, try again",
-                "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again"];
+                "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Quit asking me questions!"];
             
 window.onload = function(){
     var eight = document.getElementById("eight");
